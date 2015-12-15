eca42166821180c6c48d658680bc4dae385b5573
Tobias Fischer <t.fischer@imperial.ac.uk>
2015-12-15 14:26:16 +0000

    Bugfix

--- modules/linearClassifierModule/src/linearClassifierThread.cpp
+++ modules/linearClassifierModule/src/linearClassifierThread.cpp
@@ -139,7 +139,7 @@ void linearClassifierThread::run()
     while (!isStopping())
     {
         Bottle *p=featuresPort.read();
-        if (p==NULL)
+        if (p==NULL) {
             continue;
         }
 
